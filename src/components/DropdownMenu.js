d1b902902ca09b6705e0beffb788efdd1aba81c6
ekinomicss <zorerekin@gmail.com>
2024-09-16 18:07:42 -0400

    make new logs

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -8,31 +8,24 @@ function DropdownMenu() {
   };
 
   return (
-    <div>
-      <select onChange={handleChange} value={selectedHippo}>
+    <div className="dropdown-container">
+      <select 
+        className="modern-dropdown"
+        onChange={handleChange} 
+        value={selectedHippo}
+      >
         <option value="" disabled>Select a Baby Hippo</option>
         <option value="moo_deng_1">Moo Deng 1</option>
-        <option value="moo_deng_2">Moo Deng 2</option>
         {/* Add more options here if needed */}
       </select>
 
       {selectedHippo === 'moo_deng_1' && (
-        <div style={{ marginTop: '20px' }}>
+        <div className="image-container">
           <img 
             src="/images/moo_deng_1.jpeg" 
             alt="Moo Deng 1" 
-            style={{ maxWidth: '800px' }}
+            className="hippo-image"
           />
-        </div>
-      )}
-
-    {selectedHippo === 'moo_deng_2' && (
-        <div style={{ marginTop: '20px' }}>
-        <img 
-            src="/images/moo_deng_2.jpeg" 
-            alt="Moo Deng 2" 
-            style={{ maxWidth: '800px' }}
-        />
         </div>
       )}
     </div>
