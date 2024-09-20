ae7a1b27b5ca3e8ce6ec302dc364c40c046bde3e
ekinomicss <zorerekin@gmail.com>
2024-09-20 13:40:12 -0400

    logging server and client side errors on kibana+es now

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,37 @@
-import '../styles/globals.css'; 
+import '../styles/globals.css';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // For client-side errors
+    if (typeof window !== 'undefined') {
+      window.onerror = function (message, source, lineno, colno, error) {
+        fetch('/api/logError', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            message,
+            source,
+            lineno,
+            colno,
+            stack: error ? error.stack : null,
+          }),
+        });
+      };
+    }
+  }, []); 
+
   return (
     <>
       <Head>
-        <title>Baby Hippo Central</title>            
+        <title>Baby Hippo Central</title>
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
+
 export default MyApp;
