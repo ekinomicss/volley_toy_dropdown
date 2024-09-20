d3cee3126d093a67c0ccadf5ee2ef3c37a429d55
ekinomicss <zorerekin@gmail.com>
2024-09-20 13:49:20 -0400

    add more pics

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -18,15 +18,34 @@ function DropdownMenu() {
         value={selectedHippo}
       >
         <option value="" disabled>Select a Baby Hippo</option>
-        <option value="moo_deng_1">Moo Deng 1</option>
-        {/* Add more options here if needed */}
+        <option value="moo_deng_1">Moo Deng: Cute</option>
+        <option value="moo_deng_2">Moo Deng: Grab</option>
+        <option value="moo_deng_3">Moo Deng: Attack</option>
       </select>
 
       {selectedHippo === 'moo_deng_1' && (
         <div className="image-container">
           <img 
-            src="/public/images/moo_deng_1.jpeg" 
+            src="/images/moo_deng_1.jpeg" 
             alt="Moo Deng 1" 
+            className="hippo-image"
+          />
+        </div>
+      )}
+      {selectedHippo === 'moo_deng_2' && (
+        <div className="image-container">
+          <img 
+            src="/images/moo_deng_1.jpeg" 
+            alt="Moo Deng 2" 
+            className="hippo-image"
+          />
+        </div>
+      )}
+      {selectedHippo === 'moo_deng_3' && (
+        <div className="image-container">
+          <img 
+            src="/images/moo_deng_3.jpeg" 
+            alt="Moo Deng 3" 
             className="hippo-image"
           />
         </div>
